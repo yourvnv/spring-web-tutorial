f64241bf0152a3b4d2ab6a7d709359d566db5024
yourvnv <yournv@gmail.com>
2019-01-06 17:32:30 +0800

    1.4 Spring web with home controller

---
+++ src/main/java/io/github/yourvnv/controller/HomeController.java
@@ -0,0 +1,16 @@
+package io.github.yourvnv.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ResponseBody
+    public String index() {
+        return "Hello world";
+    }
+}
