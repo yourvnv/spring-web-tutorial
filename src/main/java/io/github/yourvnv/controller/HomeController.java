2c6262f55fa44cac5c9ecff01657455aca53127b
yourvnv <yournv@gmail.com>
2019-01-06 17:34:51 +0800

    1.5 Spring web @RestController

--- src/main/java/io/github/yourvnv/controller/HomeController.java
+++ src/main/java/io/github/yourvnv/controller/HomeController.java
@@ -1,15 +1,13 @@
 package io.github.yourvnv.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    @ResponseBody
+    @GetMapping
     public String index() {
         return "Hello world";
     }
